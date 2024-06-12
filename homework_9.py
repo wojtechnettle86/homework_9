6f902804c99eee32a4bcc49f78905d85b02f7c54
wojtechnettle86 <wojtech.nettle@gmail.com>
2024-06-12 16:11:50 +0200

    Update homework_9.py
    task 2

--- homework_9.py
+++ homework_9.py
@@ -9,3 +9,17 @@ def calculate_product(list):
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 result = calculate_product(numbers)
 print(f"Součin všech čísel v seznamu je: {result}")
+
+
+#task 2
+def minimum(list):
+    minimum = list[0]
+    for num in list[1:]:
+        if num < minimum:
+            minimum = num
+    return minimum
+
+
+numbers = [8, 15, 2, 8, 10, 42, 65]
+result = minimum(numbers)
+print(f"Minimální hodnota čísla z listu je: {result}")
